49eafc0c8f5e045d5c1c158baebbf6302afbd2d3
RoshanPawara54 <roshanpawara335@gmail.com>
2025-09-06 14:59:14 +0530

    Background

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-// Dark mode toggle
-const toggleBtn = document.getElementById("theme-toggle");
-const body = document.body
-toggleBtn.addEventListener("click", () => {
-  body.classList.toggle("dark-mode");
-  toggleBtn.textContent = body.classList.contains("dark-mode") ? "☀️" : "🌙";
+// VANTA Waves background
+let vantaEffect = VANTA.WAVES({
+  el: "#background",  // attach to background div
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  color: 0x1e1e1e,
+  shininess: 50.00,
+  waveHeight: 20.00,
+  waveSpeed: 0.75,
+  zoom: 1.00,
+  backgroundColor: 0x111111
 });
